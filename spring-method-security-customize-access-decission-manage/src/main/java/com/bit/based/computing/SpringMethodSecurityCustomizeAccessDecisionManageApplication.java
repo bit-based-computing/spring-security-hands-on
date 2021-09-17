2d77d50079e38b271851cd99d744bf18ef7a460f
foysal ahmmed <foysal@bitmascot.com>
2021-09-17 20:14:18 +0600

    project initial push

---
+++ spring-method-security-customize-access-decission-manage/src/main/java/com/bit/based/computing/SpringMethodSecurityCustomizeAccessDecisionManageApplication.java
@@ -0,0 +1,13 @@
+package com.bit.based.computing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMethodSecurityCustomizeAccessDecisionManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMethodSecurityCustomizeAccessDecisionManageApplication.class, args);
+    }
+
+}
